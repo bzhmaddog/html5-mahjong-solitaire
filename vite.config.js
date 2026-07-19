1a05f50c192682e727d1ee114706627055fede15
Cédric Martin <cedmart1@gmail.com>
2026-07-19 11:27:31 +0200

    Trying to fix deployment #2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const pagesBase = isGitHubActions && repoName ? `/${repoName}/` : '/';
+
 export default defineConfig({
+  base: pagesBase,
   root: '.',
   test: {
     environment: 'jsdom',
